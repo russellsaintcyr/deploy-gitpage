c4ef42c3c821a45714c8ea79fbf891a26b383b1d
Russell Saint Cyr <rcyr@creditshop.com>
2024-12-10 23:17:43 -0500

    Add web-api-ts-sdk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SpotifyApi } from '@spotify/web-api-ts-sdk';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,26 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'deploy-gitpage';
+
+  constructor() {
+    // const sdk = SpotifyApi.withUserAuthorization("client-id", "https://localhost:4200", ["scope1", "scope2"]);
+    console.log('AppComponent constructor')
+    this.init();
+  }
+
+  private async init() {
+    console.log('AppComponent init')
+    const api = SpotifyApi.withClientCredentials(
+      "your-client-id",
+      "your-client-secret"
+    );
+
+    const items = await api.search("The Beatles", ["artist"]);
+
+    console.table(items.artists.items.map((item: any) => ({
+      name: item.name,
+      followers: item.followers.total,
+      popularity: item.popularity,
+    })));
+  }
 }
